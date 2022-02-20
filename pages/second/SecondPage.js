606f14f17792f4d6e407d0b3aabdaa6007d91b7e
Ruben <ru.abelyan@mail.ru>
2022-02-20 16:41:25 +0400

    added Map cordinats

--- pages/second/SecondPage.js
+++ pages/second/SecondPage.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import css from "./SecondPage.module.css";
 import img from "../images/search_2.png";
 import menu from "../images/menu.png";
 import list from "../images/list.png";
 import geo from "../images/geolocation.png";
-import { Map, Placemark, YMaps } from "react-yandex-maps";
+import { Map, Placemark, Circle, YMaps } from "react-yandex-maps";
 import Image from "next/image";
 import backIcon from "../svg/back.png";
 import searchIcon from "../svg/search.png";
 import ArrowPrevTailIcon from "../svg/back.svg";
 
 const SecondPage = () => {
+  const [position, setPosition] = useState([]);
+  const [radius, setRadius] = useState(1000);
+
+  const RADIUS = 1500;
+  const circle = useRef(null);
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    setPosition([coords.latitude, coords.longitude]);
+  }, console.log("error"));
+
+  // console.log(position);
   return (
     <div className={css.main}>
       <div className={css.secondPage}>
@@ -21,44 +31,52 @@ const SecondPage = () => {
             </div>
             <div className={css.firstSearchBlock}>
               <input type="text" placeholder={"Մուտքագրեք որոնման տվյալները"} />
-              <div
-                style={{
-                  position: "absolute",
-                  top: "23px",
-                  right: "10px",
-                  height: "50px",
-                  width: "50px",
-                }}
-              >
+              <div className={css.inputStyles}>
                 <ArrowPrevTailIcon />
               </div>
             </div>
             <div className={css.secondSearchBlock}>
-              <input type="text" placeholder={"Մուտքագրեք շաառավիղը"} />
-              <button>Որոնել</button>
+              <input
+                type="text"
+                onChange={({ target }) => setRadius(+target.value)}
+                placeholder={"Մուտքագրեք շաառավիղը"}
+              />
+              <button onClick={()=> setRadius(+target.value)}>Որոնել</button>
             </div>
             <div className={css.map}>
               <YMaps>
                 <Map
                   style={{ with: "80%", height: "72%", paddingTop: "10px" }}
-                  defaultState={{ center: [55.75, 37.57], zoom: 9 }}
-                  instanceRef={(inst) => {
-                    inst.events.add("click", console.log);
-                  }}
+                  defaultState={{ center: position, zoom: 13 }}
+                  // instanceRef={(inst) => {
+                  //   inst.events.add("click", console.log);
+                  // }}
                 >
                   <Placemark
-                    geometry={[55.75, 37.57]}
+                    geometry={position}
                     instanceRef={(inst) => {
                       console.log(
                         ":rocket: ~ file: App.js ~ line 22 ~ App ~ inst",
                         inst
                       );
-                      inst.events.add("click", (e) => {
-                        console.log(
-                          ":rocket: ~ file: App.js ~ line 26 ~ App ~ e",
-                          e
-                        );
-                      });
+                      inst &&
+                        inst.events.add("click", (e) => {
+                          console.log(
+                            ":rocket: ~ file: App.js ~ line 26 ~ App ~ e",
+                            e
+                          );
+                        });
+                    }}
+                  />
+                  <Circle
+                    geometry={[position, radius]}
+                    defaultGeometry={[position, RADIUS]}
+                    options={{
+                      draggable: false,
+                      fillColor: "#8998a377",
+                      strokeColor: "#8998a3",
+                      strokeOpacity: 0.9,
+                      strokeWidth: 2,
                     }}
                   />
                 </Map>
